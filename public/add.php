2bb5a5f41c87537a1a6ce3edcf68f8eb4304bed0
khanhdc24v7k515-rgb <khanhdc24v7k515@vlvh.ctu.edu.vn>
2026-08-16 19:46:38 +0700

    Cai dat them moi contact

--- public/add.php
+++ public/add.php
@@ -1,71 +1,96 @@
 <?php
 require_once __DIR__ . '/../src/bootstrap.php';
+use CT275\Labs\Contact;
 
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $contactData = [
+        'name' => $_POST['name'] ?? '',
+        'phone' => $_POST['phone'] ?? '',
+        'notes' => $_POST['notes'] ?? '',
+    ];
+
+    $contact = new Contact($PDO);
+    $errors = $contact->validate($contactData);
+    if (empty($errors)) {
+        $contact->fill($contactData);
+        $contact->save() && redirect('/');
+    }
+}
 include_once __DIR__ . '/../src/partials/header.php';
 ?>
 
 <body>
-  <?php include_once __DIR__ . '/../src/partials/navbar.php' ?>
+    <?php include_once __DIR__ . '/../src/partials/navbar.php' ?>
 
-  <!-- Main Page Content -->
-  <div class="container">
+    <!-- Main Page Content -->
+    <div class="container">
 
-    <?php
+        <?php
     $subtitle = 'Add your contacts here.';
     include_once __DIR__ . '/../src/partials/heading.php';
     ?>
 
-    <div class="row">
-      <div class="col-12">
+        <div class="row">
+            <div class="col-12">
 
-        <form method="post" class="col-md-6 offset-md-3">
+                <form method="post" class="col-md-6 offset-md-3">
 
-          <!-- Name -->
-          <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" name="name" class="form-control<?= isset($errors['name']) ? ' is-invalid' : '' ?>" maxlen="255" id="name" placeholder="Enter Name" value="<?= isset($_POST['name']) ? html_escape($_POST['name']) : '' ?>" />
+                    <!-- Name -->
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" name="name"
+                            class="form-control<?= isset($errors['name']) ? ' is-invalid' : '' ?>" maxlen="255"
+                            id="name" placeholder="Enter Name"
+                            value="<?= isset($_POST['name']) ? html_escape($_POST['name']) : '' ?>" />
 
-            <?php if (isset($errors['name'])) : ?>
-              <span class="invalid-feedback">
-                <strong><?= $errors['name'] ?></strong>
-              </span>
-            <?php endif ?>
-          </div>
+                        <?php if (isset($errors['name'])) : ?>
+                        <span class="invalid-feedback">
+                            <strong><?= $errors['name'] ?></strong>
+                        </span>
+                        <?php endif ?>
+                    </div>
 
-          <!-- Phone -->
-          <div class="mb-3">
-            <label for="phone" class="form-label">Phone Number</label>
-            <input type="text" name="phone" class="form-control<?= isset($errors['phone']) ? ' is-invalid' : '' ?>" maxlen="255" id="phone" placeholder="Enter Phone" value="<?= isset($_POST['phone']) ? html_escape($_POST['phone']) : '' ?>" />
+                    <!-- Phone -->
+                    <div class="mb-3">
+                        <label for="phone" class="form-label">Phone Number</label>
+                        <input type="text" name="phone"
+                            class="form-control<?= isset($errors['phone']) ? ' is-invalid' : '' ?>" maxlen="255"
+                            id="phone" placeholder="Enter Phone"
+                            value="<?= isset($_POST['phone']) ? html_escape($_POST['phone']) : '' ?>" />
 
-            <?php if (isset($errors['phone'])) : ?>
-              <span class="invalid-feedback">
-                <strong><?= $errors['phone'] ?></strong>
-              </span>
-            <?php endif ?>
-          </div>
+                        <?php if (isset($errors['phone'])) : ?>
+                        <span class="invalid-feedback">
+                            <strong><?= $errors['phone'] ?></strong>
+                        </span>
+                        <?php endif ?>
+                    </div>
 
-          <!-- Notes -->
-          <div class="mb-3">
-            <label for="notes" class="form-label">Notes </label>
-            <textarea name="notes" id="notes" class="form-control<?= isset($errors['notes']) ? ' is-invalid' : '' ?>" placeholder="Enter notes (maximum character limit: 255)"><?= isset($_POST['notes']) ? html_escape($_POST['notes']) : '' ?></textarea>
+                    <!-- Notes -->
+                    <div class="mb-3">
+                        <label for="notes" class="form-label">Notes </label>
+                        <textarea name="notes" id="notes"
+                            class="form-control<?= isset($errors['notes']) ? ' is-invalid' : '' ?>"
+                            placeholder="Enter notes (maximum character limit: 255)"><?= isset($_POST['notes']) ? html_escape($_POST['notes']) : '' ?></textarea>
 
-            <?php if (isset($errors['notes'])) : ?>
-              <span class="invalid-feedback">
-                <strong><?= $errors['notes'] ?></strong>
-              </span>
-            <?php endif ?>
-          </div>
+                        <?php if (isset($errors['notes'])) : ?>
+                        <span class="invalid-feedback">
+                            <strong><?= $errors['notes'] ?></strong>
+                        </span>
+                        <?php endif ?>
+                    </div>
 
-          <!-- Submit -->
-          <button type="submit" name="submit" class="btn btn-primary">Add Contact</button>
-        </form>
+                    <!-- Submit -->
+                    <button type="submit" name="submit" class="btn btn-primary">Add Contact</button>
+                </form>
 
-      </div>
+            </div>
+        </div>
+
     </div>
 
-  </div>
-
-  <?php include_once __DIR__ . '/../src/partials/footer.php' ?>
+    <?php include_once __DIR__ . '/../src/partials/footer.php' ?>
 </body>
 
 </html>
